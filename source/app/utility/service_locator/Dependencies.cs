3b925a27e54c88133ba40454bbd17fcd063abc1d
Keivan Beigi <keivanb@iqmetrix.com>
2012-11-23 10:11:17 -0800

    added some registration

--- source/app/utility/service_locator/Dependencies.cs
+++ source/app/utility/service_locator/Dependencies.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Compilation;
 using app.web.application.catalogbrowsing;
+using app.web.application.stubs;
 using app.web.core;
 using app.web.core.aspnet;
 using app.web.core.stubs;
@@ -29,6 +30,18 @@ namespace app.utility.service_locator
                           new RequestCommand(request => request.path.Contains("subdepartment") , c.an<ViewAReport<ViewSubDepartmentsRequest>>()),
                           new RequestCommand(request => request.path.Contains("products"), c.an<ViewAReport<ViewProductsInDepartmentRequest>>()),
                       });
+
+                      container.register<IDisplayInformation>(c => new WebFormsDisplayEngine(c.an<ICreateViews>(), c.an<IGetTheCurrentlyExecutingRequest>()));
+
+/*                      container.register(c => new ViewAReport<ViewMainDepartmentRequest>(c.an<IDisplayInformation>(),
+                                                                                    input =>
+                                                                                        
+                                                                                          new StubCatalog().get_the_main_departments
+                                                                                        ));*/
+
+                      //container.register<ViewAReport<ViewMainDepartmentRequest>>(c=> ));
+
+
 
                       container.register<IGetTheCurrentlyExecutingRequest>(c => () => HttpContext.Current);
                       container.register<ICreateViews>(c => new ViewFactory(BuildManager.CreateInstanceFromVirtualPath, container.an<StubPathRegistry>()));
